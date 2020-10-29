2c52138352449b8138294c3fe480ec97888bcbe1
JeongPyo <jeongpyo_park@tmax.co.kr>
2020-10-29 17:23:26 +0900

    props & state 사용

--- src/App.js
+++ src/App.js
@@ -6,123 +6,31 @@ import UpdateContent from "./components/UpdateContent"
 import Subject from "./components/Subject"
 import Control from "./components/Control"
 import './App.css';
+import Content from './components/Content';
 
 
 class App extends Component {
-  constructor(props){
+
+  // 생성자는 컴포넌트가 생성될 때, render 보다 먼저 호출되어 컴포넌트의 값을 초기화 시켜주는 역할을 한다.
+  constructor(props) {
     super(props);
-    this.max_content_id = 3;
     this.state = {
-      mode:'welcome',
-      selected_content_id:2,
-      subject:{title:'WEB', sub:'World Wide Web!'},
-      welcome:{title:'Welcome', desc:'Hello, React!!'},
-      contents:[
+      subject:{title: 'WEB', sub:"world wide web!"},
+      contents: [
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
         {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
       ]
     }
-  }
-  getReadContent(){
-    var i = 0;
-      while(i < this.state.contents.length){
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id) {
-          return data;
-          break;
-        }
-        i = i + 1;
-      }
-  }
-  getContent(){
-    var _title, _desc, _article = null;
-    if(this.state.mode ===  'welcome'){
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    } else if(this.state.mode === 'read'){
-      var _content = this.getReadContent();
-      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
-    } else  if(this.state.mode === 'create'){
-      _article = <CreateContent onSubmit={function(_title, _desc){
-        this.max_content_id = this.max_content_id+1;
-        var _contents = Array.from(this.state.contents);
-        _contents.push({id:this.max_content_id, title:_title, desc:_desc});
-        this.setState({
-          contents:_contents,
-          mode:'read',
-          selected_content_id:this.max_content_id
-        });
-      }.bind(this)}></CreateContent>
-    } else  if(this.state.mode === 'update'){
-      _content = this.getReadContent();
-      _article = <UpdateContent data={_content} onSubmit={
-        function(_id, _title, _desc){
-          var _contents = Array.from(this.state.contents);
-          var i = 0;
-          while(i < _contents.length){
-            if(_contents[i].id === _id) {
-              _contents[i] = {id:_id, title:_title, desc:_desc};
-              break;
-            }
-            i = i + 1;
-          }
-          this.setState({
-            contents:_contents,
-            mode:'read'
-          });
-        }.bind(this)}></UpdateContent>
-    }
-    return _article;
+
   }
   render() {
-    console.log('App render');
     return (
       <div className="App">
-        <Subject 
-          title={this.state.subject.title} 
-          sub={this.state.subject.sub}
-          onChangePage={function(){
-            this.setState({mode:'welcome'});
-          }.bind(this)}
-        >
-        </Subject>
-        <TOC 
-          onChangePage={function(id){
-            this.setState({
-              mode:'read',
-              selected_content_id:Number(id)
-            });
-          }.bind(this)} 
-          data={this.state.contents}
-        ></TOC>
-        <Control onChangeMode={function(_mode){
-          if(_mode === 'delete'){
-            if(window.confirm('really?')){
-              var _contents = Array.from(this.state.contents);
-              var i = 0;
-              while(i < _contents.length){
-                if(_contents[i].id === this.state.selected_content_id){
-                  _contents.splice(i,1);
-                  break;
-                }
-                i = i + 1;
-              }
-              this.setState({
-                mode:'welcome',
-                contents:_contents
-              });
-              alert('deleted!');
-            }
-          } else {
-            this.setState({
-              mode:_mode
-            });
-          }
-          
-        }.bind(this)}></Control>
-        {this.getContent()}
+        {/* 상위 컴포넌트의 state 값을 하위 컴포넌트의 props의 값으로 전달하는 것 가능. */}
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
